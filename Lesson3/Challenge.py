37b4000676c634e9080dbd3f3647aaa5d9d34e78
Albin-B7 <eaglealbin@gmail.com>
2026-03-07 16:39:15 +0100

    l4

---
+++ Lesson3/Challenge.py
@@ -0,0 +1,12 @@
+s_gpa = 3.5
+s_score = 65
+
+if s_gpa >= 3.5:
+    if 50 <= s_score <= 65:
+        print(f"Student with GPA {s_gpa} and test score of {s_score} may be eligible for partial " f"scholarship.")
+    elif s_score > 65:
+        print(f"Student with GPA {s_gpa} and test score of {s_score} is eligible for a full scholarship.")
+    else:
+        print(f"Student with GPA {s_gpa} and test score of {s_score} is not eligible for scholarship.")
+else:
+    print(f"Student with GPA {s_gpa} and test score of {s_score} is not eligible for sholarship")
